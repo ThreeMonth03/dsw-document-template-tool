28abf331f245af316a8f7a3118de1be0bfcc13ae
ThreeMonth03 <austin20463@gmail.com>
2026-06-23 19:02:56 +0800

    refactor: remove science europe rules shim

--- src/dsw_document_template_tool/_template_transform/science_europe.py
+++ src/dsw_document_template_tool/_template_transform/science_europe.py
@@ -2,10 +2,8 @@
 
 from __future__ import annotations
 
-from .science_europe_rules import (
-    rewrite_science_europe_balanced_source_fragments,
-    rewrite_science_europe_unbalanced_html_fragments,
-)
+from .science_europe_balanced_rules import rewrite_science_europe_balanced_source_fragments
+from .science_europe_unbalanced_rules import rewrite_science_europe_unbalanced_html_fragments
 
 __all__ = [
     "rewrite_science_europe_balanced_source_fragments",
